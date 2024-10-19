54c08821fa35e1d06f3c1812247e41c881ce529f
Julio Francisco Bernardino <juliobernardinoprogramador@gmail.com>
2024-10-19 12:26:24 -0300

    Update Bd-retifica.sql

--- Bd-retifica.sql
+++ Bd-retifica.sql
@@ -20,7 +20,7 @@ CREATE TABLE endereco(
 CREATE TABLE servico(
 	idservico INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	nomeservico VARCHAR(40) NOT NULL,
-	preco NUMERIC(10,2) CHECK(preco > 0) NOT NULL,
+	preco NUMERIC(10,2) CHECK(preco > 0) NOT NULL
 );
 
 
@@ -35,8 +35,16 @@ CREATE TABLE pedido(
 	idpedido INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	cpf VARCHAR NOT NULL REFERENCES clientes(cpf),
 	idservico INTEGER NOT NULL REFERENCES servico(idservico),
-	peca INTEGER NOT NULL REFERENCES peca(idpeca),
-	total NUMERIC(10,2) CHECK(preco > 0) NOT NULL
+	idpeca INTEGER NOT NULL REFERENCES peca(idpeca),
+	total NUMERIC(10,2) CHECK(total > 0) NOT NULL
+);
+
+CREATE TABLE pecapedido (
+    idpeca INTEGER NOT NULL,
+    idpedido INTEGER NOT NULL,
+    CONSTRAINT pk_id_peca_pedido PRIMARY KEY (idpeca, idpedido),
+    CONSTRAINT fk_id_peca FOREIGN KEY (idpeca) REFERENCES peca (idpeca) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_id_pedido FOREIGN KEY (idpedido) REFERENCES pedido (idpedido) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 

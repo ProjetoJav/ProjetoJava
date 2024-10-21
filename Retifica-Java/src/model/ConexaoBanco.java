139ed45f789295c1f21011f63a30818b42e3edc0
Julio Francisco Bernardino <juliobernardinoprogramador@gmail.com>
2024-10-21 18:40:19 -0300

    Atualização1

--- Retifica-Java/src/model/ConexaoBanco.java
+++ Retifica-Java/src/model/ConexaoBanco.java
@@ -1,6 +1,15 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexaoBanco {
-    
-    
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/bd-retifica";
+
+    // Método para obter a conexão com o banco de dados
+    public static Connection getConnection(String usuario, String senha) throws SQLException {
+        return DriverManager.getConnection(URL, usuario, senha);
+    }
 }
